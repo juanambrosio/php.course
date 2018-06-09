9d230a5aae0a15c1402d00822b3ed6962bd75271
Juan Ambrosio Bautista Alonso <krops.art.2@gmail.com>
2018-06-09 10:00:38 -0500

    "nueva version XML VSC"

--- prueba.php
+++ prueba.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * FUNCIONES CALLBACK
- */
+
 
 
     function funCustom(string $data, callable $callback)
@@ -31,4 +31,24 @@
     }
 
 
-createFile("Hola");
+createFile("Hola");*/
+function GetString(string  $texto, string $urlLang = "es")
+{
+
+    $i = $urlLang=="es"?0:1;
+    $traslate = $urlLang=="es"?1:0;
+
+    $fr = fopen('/var/www/html/traslate.csv', 'r');
+    while(false !== ( $line = fgetcsv($fr)) ){
+        $line[1] = $line[1] ?? null;
+
+        if($line[$i] === $texto) {
+
+            echo($line[$traslate]);
+        }
+
+    }
+}
+
+
+GetString("hola","es");
